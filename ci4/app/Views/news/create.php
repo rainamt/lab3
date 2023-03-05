81ff2673e87acf398fe647c12615f31c35a2c7e8
rterania <rterania@apc.edu.ph>
2023-03-05 13:15:32 +0800

    fix

--- ci4/app/Views/news/create.php
+++ ci4/app/Views/news/create.php
@@ -1,3 +1,22 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>  
+<ul class="tabs">
+    <li><a href="/~rterania/lab3/ci4/public/index#contact">Home</a></li>
+        <li><a href="/~rterania/lab3/ci4/public/index#contact">Hobbies</a></li>
+        <li><a href="/~rterania/lab3/ci4/public/index#contact">Links</a></li>
+    
+  </ul>
+   <div style = "text-align: center;
+    color:rgb(85, 19, 19);">
+<br><br><br><br>
+
+
+
+
 <h2><?= esc($title) ?></h2>
 
 <?= session()->getFlashdata('error') ?>
@@ -16,3 +35,7 @@
 
     <input type="submit" name="submit" value="Create news item">
 </form>
+
+
+</body>
+</html>
--- ci4/app/Views/news/index.php
+++ ci4/app/Views/news/index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>  
+<ul class="tabs">
+    <li><a href="/~rterania/lab3/ci4/public/index#contact">Home</a></li>
+        <li><a href="/~rterania/lab3/ci4/public/index#contact">Hobbies</a></li>
+        <li><a href="/~rterania/lab3/ci4/public/index#contact">Links</a></li>
+    
+  </ul>
+   <div style = "text-align: center;
+    color:rgb(85, 19, 19);">
+<br><br><br><br>
+
+
+
+
+
+
+
 <h2><?= esc($title) ?></h2>
 
 <?php if (! empty($news) && is_array($news)): ?>
@@ -20,3 +42,8 @@
     <p>Unable to find any news for you.</p>
 
 <?php endif ?>
+
+
+
+</body>
+</html>
