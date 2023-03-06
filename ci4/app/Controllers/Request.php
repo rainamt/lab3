b54f35368c2800fa1d06a2af4e8e452f978ce9b1
rterania <rterania@apc.edu.ph>
2023-03-06 17:10:27 +0800

    fix

--- ci4/app/Controllers/Request.php
+++ ci4/app/Controllers/Request.php
@@ -12,30 +12,12 @@ class Request extends BaseController
 
 		$data = [
             'request'  => $model->getRequest(),
-            'title' => 'Request',
+            'title' => 'Requestbook',
         ];
 
         return view('templates/header', $data)
              . view('request/index')
              . view('templates/footer');
-    }
-
-
-    public function view($slug = null)
-    {
-        $model = model(RequestModel::class);
-
-        $data['request'] = $model->getRequest($slug);
-
-        if (empty($data['request'])) {
-            throw new PageNotFoundException('NO REQUEST ENTRY: ' . $slug);
-        }
-
-        $data['title'] = $data['request']['title'];
-
-        return view('templates/header', $data)
-            . view('request/view')
-            . view('templates/footer');
     }
 	
 	public function create()
@@ -45,23 +27,23 @@ class Request extends BaseController
         // Checks whether the form is submitted.
         if (! $this->request->is('post')) {
             // The form is not submitted, so returns the form.
-            return view('templates/header', ['title' => 'REQUEST FORM'])
+            return view('templates/header', ['title' => 'Create a request entry'])
                 . view('request/create')
                 . view('templates/footer');
         }
 
-        $post = $this->request->getPost(['firstname', 'email', 'payment', 'comment', 'style']);
+        $post = $this->request->getPost(['name', 'email', 'payment', 'comment', 'style']);
 
         // Checks whether the submitted data passed the validation rules.
         if (! $this->validateData($post, [
-            'firstname' => 'required|max_length[255]|min_length[3]',
+            'name' => 'required|max_length[255]|min_length[3]',
             'email' => 'required|max_length[255]|min_length[3]',
             'payment' => 'required|max_length[255]|min_length[3]',			
             'comment'  => 'required|max_length[5000]|min_length[10]',
             'style' => 'required|max_length[255]|min_length[3]',			
         ])) {
             // The validation fails, so returns the form.
-            return view('templates/header', ['title' => 'REQUEST FORM'])
+            return view('templates/header', ['title' => 'Add a request entry'])
                 . view('request/create')
                 . view('templates/footer');
         }
@@ -69,14 +51,14 @@ class Request extends BaseController
         $model = model(RequestModel::class);
 
         $model->save([
-            'firstname' => $post['firstname'],
+            'name' => $post['name'],
             'email'  => $post['email'],
             'payment'  => $post['payment'],
             'comment'  => $post['comment'],
             'style'  => $post['style'],
         ]);
 
-        return view('templates/header', ['title' => 'REQUEST FORM'])
+        return view('templates/header', ['title' => 'Add a request Entry'])
             . view('request/success')
             . view('templates/footer');
     }
--- ci4/app/Models/RequestModel.php
+++ ci4/app/Models/RequestModel.php
@@ -4,19 +4,15 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class RequestModel extends Model 
+class RequestModel extends Model
 {
     protected $table = 'rterania_request';
 
-    protected $allowedFields = ['firstname', 'email', 'payment', 'comment', 'style' ];
-    
-    public function getRequest($slug = false) 
-    { 
-        if ($slug === false) {
-            return $this->findAll();
+    protected $allowedFields = ['name', 'email', 'payment', 'comment', 'style'];
 
-        }
-        return $this->where(['slug' => $slug])->first();
+	
+	 public function getRequest()
+    {     
+        return $this->findAll();
     }
-
 }
