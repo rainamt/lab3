68a0fc721dc95b9bf1eb74ece5cf72bb52c4b349
rterania <rterania@apc.edu.ph>
2023-03-06 12:48:39 +0800

    css fix

--- ci4/app/Views/request/create.php
+++ ci4/app/Views/request/create.php
@@ -1,14 +1,29 @@
-<div class="tabs">
-    <ul>
-        <li><a href="/~rterania/lab3/ci4/public/home#home">ABOUT</a></li>
-        <li><a href="/~rterania/lab3/ci4/public/home#Hobbies">HOBBIES</a></li>
-        <li><a href="/~rterania/lab3/ci4/public/home#contact">LINKS</a></li>
-        <li><a href="request/create">REQUEST FORMS</a></li>
-        <li><a href="/~rterania/lab3/ci4/public/request">REQUEST QUEUE</a></li>
-      
+<!DOCTYPE html>
+<html lang= "en" >
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="index.css">
 
-          <p id="demo"></p></li>
-      </ul>
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="index.js" defer></script>
+
+
+<title> ꕥ a very normal site </title>
+</head>
+<body>
+
+  
+  <ul class="tabs">
+    <li><a href="/~rterania/lab3/ci4/public/index#home">Home</a></li>
+        <li><a href="/~rterania/lab3/ci4/public/index#Hobbies">Hobbies</a></li>
+        <li><a href="/~rterania/lab3/ci4/public/index#contact">Links</a></li>
+    
+  </ul>
+
   </div>
 <br><br><br><br><br><br><br><br><br><br><br>
 <h2><?= esc($title) ?></h2>
@@ -29,3 +44,9 @@
 
     <input type="submit" name="submit" value="Create entry">
 </form>
+
+
+
+
+</body>
+</html>
--- ci4/app/Views/request/success.php
+++ ci4/app/Views/request/success.php
@@ -1,16 +1,37 @@
-<div class="tabs">
-    <ul>
-        <li><a href="/~rterania/lab3/ci4/public/home#home">ABOUT</a></li>
-        <li><a href="/~rterania/lab3/ci4/public/home#Hobbies">HOBBIES</a></li>
-        <li><a href="/~rterania/lab3/ci4/public/home#contact">LINKS</a></li>
-        <li><a href="request/create">REQUEST FORMS</a></li>
-        <li><a href="/~rterania/lab3/ci4/public/request">REQUEST QUEUE</a></li>
-      
+<!DOCTYPE html>
+<html lang= "en" >
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="index.css">
 
-          <p id="demo"></p></li>
-      </ul>
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="index.js" defer></script>
+
+
+<title> ꕥ a very normal site </title>
+</head>
+<body>
+
+  
+  <ul class="tabs">
+    <li><a href="/~rterania/lab3/ci4/public/index#home">Home</a></li>
+        <li><a href="/~rterania/lab3/ci4/public/index#Hobbies">Hobbies</a></li>
+        <li><a href="/~rterania/lab3/ci4/public/index#contact">Links</a></li>
+    
+  </ul>
+
   </div>
 <br><br><br><br><br><br><br><br><br><br><br>
 <p>Request has been submitted.</p>
 <br><br><br><br>
 <p>Will be reviewed shortly</p>
+
+
+
+
+</body>
+</html>
